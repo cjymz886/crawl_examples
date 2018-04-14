74ee529e70a67dbd031ac9d2cb30a80de9c0787d
Darral <827292744@qq.com>
2018-04-14 09:39:29 +0800

    Update pipelines.py

--- examples/tutorial/tutorial/pipelines.py
+++ examples/tutorial/tutorial/pipelines.py
@@ -5,7 +5,7 @@ import codecs
 
 class TutorialPipeline(object):
         def __init__(self):
-            self.file = codecs.open('M_ahospital.txt', 'w', encoding='utf-8')
+            self.file = codecs.open('ahospital.txt', 'w', encoding='utf-8')
 
         def process_item(self, item, spider):
             line = json.dumps(dict(item), ensure_ascii=False) + "\n"
